25880f335224134a7eb51343a9a03c0fcb9d0fec
gowtham1723 <53259528+gowtham1723@users.noreply.github.com>
2019-07-27 22:18:28 +0530

    Add files via upload

---
+++ adminlogin.java
@@ -0,0 +1,88 @@
+package com.example.supermarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import androidx.annotation.NonNull;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class adminlogin extends AppCompatActivity {
+    public EditText loginEmailId, logInpasswd;
+    Button btnLogIn;
+    TextView signup;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminlogin);
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginEmailId = findViewById(R.id.loginEmail);
+        logInpasswd = findViewById(R.id.loginpaswd);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(adminlogin.this, "User logged in ", Toast.LENGTH_SHORT).show();
+                    Intent I = new Intent(adminlogin.this, Adminactivity.class);
+                    startActivity(I);
+                } else {
+                    Toast.makeText(adminlogin.this, "Login to continue", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = loginEmailId.getText().toString();
+                String userPaswd = logInpasswd.getText().toString();
+                if (userEmail.isEmpty()) {
+                    loginEmailId.setError("Provide your Email first!");
+                    loginEmailId.requestFocus();
+                } else if (userPaswd.isEmpty()) {
+                    logInpasswd.setError("Enter Password!");
+                    logInpasswd.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(adminlogin.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(adminlogin.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(adminlogin.this, "Not sucessfull", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(adminlogin.this, Adminactivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(adminlogin.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+}
+
